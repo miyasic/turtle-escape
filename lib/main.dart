02c748deaf95a75e1fa2e189998effef57c87a42
kazuki_kimura <kazukidddd836@gmail.com>
2024-01-26 08:10:16 +0900

    Update main.dart to use GameApp instead of MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ggc/presentation/game_app.dart';
 import 'package:ggc/router.dart';
 import 'package:ggc/scaffold_messenger.dart';
 import 'package:ggc/theme.dart';
@@ -6,7 +7,7 @@ import 'package:ggc/util/provider_logger.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(observers: [ProviderLogger()], child: const MyApp()));
+  runApp(ProviderScope(observers: [ProviderLogger()], child: const GameApp()));
 }
 
 class MyApp extends ConsumerWidget {
